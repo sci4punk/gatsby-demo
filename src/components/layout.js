66071f8ad85083ed373a919794f3c5ac41bac05c
confidenti-al <steve@confidenti.al>
2019-09-25 20:31:45 -0400

    Playing with layouts and css classNames

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "./link";
+import layoutStyles from "./layout.module.css";
 
 const Layout = props => {
   return (
   <div>
-  <header>
+  <header className={layoutStyles.header}>
     <Link to="/">Home</Link>
     <Link to="/about">About</Link>
   </header>
